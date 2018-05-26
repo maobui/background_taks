82b0ea90b5dd4dbad305ea44f3dfa8c7ba9dcf51
mao.buidinh <mao.buidinh@hotmail.com>
2018-05-26 23:11:14 +0700

    - fix don't show charging when in foreground.

--- app/src/main/java/com/me/bui/hydrationreminder/MainActivity.java
+++ app/src/main/java/com/me/bui/hydrationreminder/MainActivity.java
@@ -20,6 +20,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.os.BatteryManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -149,6 +151,18 @@ public class MainActivity extends AppCompatActivity implements
     protected void onResume() {
         super.onResume();
         registerReceiver(mChargingReceiver, mChargingIntentFilter);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            BatteryManager batteryManager = (BatteryManager) getSystemService(BATTERY_SERVICE);
+            showCharging(batteryManager.isCharging());
+        } else {
+            IntentFilter intentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+            Intent intentBatteryStatus = registerReceiver(null, intentFilter);
+            int batteryStatus = intentBatteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+            boolean isCharging = batteryStatus == BatteryManager.BATTERY_STATUS_CHARGING ||
+                    batteryStatus == BatteryManager.BATTERY_STATUS_FULL;
+            showCharging(isCharging);
+        }
     }
 
     @Override
